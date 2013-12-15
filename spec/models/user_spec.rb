668e1260235b692ff4289e76b6b30ab354b11437
Jeremy Mitchell <jeremy@flexdevelopers.com>
2013-12-15 16:09:39 -0700

    finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,7 +30,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user@foo...com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -56,6 +56,16 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "when email has mixed case" do
+    let(:mixed_case_email) { 'fooBar@Baz.Com' }
+
+    it "should be saved as lowercase" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 
   describe "when password is not present" do
